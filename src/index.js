cdbcab3757434ebb2079807fdcaf59213867cb96
itswagi <43064115+itswagi@users.noreply.github.com>
2020-12-21 17:08:43 +0500

    Configured Prisma with GraphQL

--- src/index.js
+++ src/index.js
@@ -1,31 +1,26 @@
 const { ApolloServer } = require('apollo-server')
+const { PrismaClient } = require('@prisma/client')
 const fs = require('fs')
 const path = require('path')
 
-let links = [{
-    id: 'link-0',
-    url: 'www.gotographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}]
-let idCount = links.length
+const prisma = new PrismaClient()
+
 const resolvers = {
     Query: {
         info: () => `This is the API of Hackernews Clone`,
-        feed: () => links,
-        link: (parent, args) => {
-            const link =  links.find(link => link.id === args.id)
-            return link;
-          }
+        feed: async (parent, args, context, info) => {
+            return context.prisma.link.findMany()
+          },
     },
     Mutation: {
-        post: (parent, args) => {
-            const link = {
-             id: `link-${idCount++}`,
-             description: args.description,
-             url: args.url,
-           }
-           links.push(link)
-           return link
+        post: (parent, args, context) => {
+            const newLink = context.prisma.link.create({
+              data: {
+                url: args.url,
+                description: args.description,
+              },
+            })
+            return newLink
         },
     }
 }
@@ -35,6 +30,9 @@ const server = new ApolloServer({
         path.join(__dirname, 'schema.graphql'), 'utf8'
     ),
     resolvers,
+    context: {
+        prisma,
+    }
 })
 
 server
