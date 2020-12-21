52eb9374c2ed3042802742cbfb1b3dec55b5825f
itswagi <43064115+itswagi@users.noreply.github.com>
2020-12-21 19:40:58 +0500

    Added Prisma

--- src/index.js
+++ src/index.js
@@ -1,23 +1,39 @@
 const { ApolloServer } = require('apollo-server');
+const fs = require('fs');
+const path = require('path');
 
-// 1
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+}]
 
-// 2
+let idCount = links.length
+
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
-  }
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Mutation: {
+    post: (parent, args) => {
+       const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    }
+  },
 }
 
-// 3
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+    typeDefs: fs.readFileSync(
+        path.join(__dirname, 'schema.graphql'),
+        'utf8'
+        ),
+    resolvers,
 })
 
 server
