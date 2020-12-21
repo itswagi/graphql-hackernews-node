4e4247825a0bbd601e1d6bbd47fecbfe5c3de67c
itswagi <43064115+itswagi@users.noreply.github.com>
2020-12-21 20:32:55 +0500

    Completed Backend GraphQL Implementation

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer, PubSub } = require('apollo-server');
 const { PrismaClient } = require('@prisma/client')
 const { getUserId } = require('./utils');
 const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const User = require('./resolvers/User')
 const Link = require('./resolvers/Link')
+const Vote = require('./resolvers/Vote')
 const fs = require('fs');
 const path = require('path');
+const Subscription = require('./resolvers/Subscription')
 
 const prisma = new PrismaClient()
+const pubsub = new PubSub()
 
 const resolvers = {
     Query,
     Mutation,
+    Subscription,
     User,
-    Link
+    Link,
+    Vote
 }
 
 const server = new ApolloServer({
@@ -27,6 +32,7 @@ const server = new ApolloServer({
         return {
           ...req,
           prisma,
+          pubsub,
           userId:
             req && req.headers.authorization
               ? getUserId(req)
